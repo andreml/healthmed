e2fa8f1aa62f0e11fe7367f1dd8c73bfa9d8167e
andremunizdelima <andremunizdelima@hotmail.com>
2024-07-29 22:31:12 -0300

    Comentando classes de testes

--- tests/HealthMed.UnitTests/Fixtures/QuestionarioTestFixture.cs
+++ tests/HealthMed.UnitTests/Fixtures/QuestionarioTestFixture.cs
@@ -1,53 +1,53 @@
-﻿using Bogus;
-using HealthMed.Application.Dto;
-using Xunit;
+﻿//using Bogus;
+//using HealthMed.Application.Dto;
+//using Xunit;
 
-namespace HealthMed.UnitTests.Fixtures;
+//namespace HealthMed.UnitTests.Fixtures;
 
-public class QuestionarioTestFixture
-{
-    private readonly Faker _faker;
+//public class QuestionarioTestFixture
+//{
+//    private readonly Faker _faker;
 
-    public QuestionarioTestFixture()
-    {
-        _faker = new Faker();
-    }
+//    public QuestionarioTestFixture()
+//    {
+//        _faker = new Faker();
+//    }
 
-    public AlterarQuestionarioDto GerarAlterarQuestionarioDto() =>
-        new()
-        {
-            UsuarioId = _faker.Random.Guid(),
-            Id = _faker.Random.Guid(),
-            Nome = _faker.Random.String2(200),
-            Perguntas = new List<Guid>
-            {
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid()
-            }
-        };
+//    public AlterarQuestionarioDto GerarAlterarQuestionarioDto() =>
+//        new()
+//        {
+//            UsuarioId = _faker.Random.Guid(),
+//            Id = _faker.Random.Guid(),
+//            Nome = _faker.Random.String2(200),
+//            Perguntas = new List<Guid>
+//            {
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid()
+//            }
+//        };
 
-    public AdicionarQuestionarioDto GerarAdicionarQuestionarioDto() =>
-        new()
-        {
-            UsuarioId = _faker.Random.Guid(),
-            Nome = _faker.Random.String2(200),
-            Perguntas = new List<Guid>
-            {
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid(),
-                _faker.Random.Guid()
-            }
-        };
-}
+//    public AdicionarQuestionarioDto GerarAdicionarQuestionarioDto() =>
+//        new()
+//        {
+//            UsuarioId = _faker.Random.Guid(),
+//            Nome = _faker.Random.String2(200),
+//            Perguntas = new List<Guid>
+//            {
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid(),
+//                _faker.Random.Guid()
+//            }
+//        };
+//}
 
 
-[CollectionDefinition("QuestionarioTestFixtureCollection")]
-public class QuestionarioTestFixtureCollection : ICollectionFixture<QuestionarioTestFixture>
-{ }
+//[CollectionDefinition("QuestionarioTestFixtureCollection")]
+//public class QuestionarioTestFixtureCollection : ICollectionFixture<QuestionarioTestFixture>
+//{ }
--- tests/HealthMed.UnitTests/Fixtures/UsuarioTestFixture.cs
+++ tests/HealthMed.UnitTests/Fixtures/UsuarioTestFixture.cs
@@ -1,41 +1,41 @@
-﻿using Bogus;
-using HealthMed.Application.Dto;
-using HealthMed.Domain.Enum;
-using Xunit;
+﻿//using Bogus;
+//using HealthMed.Application.Dto;
+//using HealthMed.Domain.Enum;
+//using Xunit;
 
-namespace HealthMed.UnitTests.Fixtures;
+//namespace HealthMed.UnitTests.Fixtures;
 
-public class UsuarioTestFixture
-{
-    private readonly Faker _faker;
+//public class UsuarioTestFixture
+//{
+//    private readonly Faker _faker;
 
-    public UsuarioTestFixture()
-    {
-        _faker = new Faker();
-    }
+//    public UsuarioTestFixture()
+//    {
+//        _faker = new Faker();
+//    }
 
-    public AlterarUsuarioDto GerarAlterarUsuarioDto() =>
-        new()
-        {
-            Cpf = _faker.Random.Long(11111111111, 99999999999).ToString(),
-            Id = _faker.Random.Guid(),
-            Login = _faker.Random.String2(30),
-            Nome = _faker.Random.String2(30),
-            Senha = _faker.Random.String2(10)
-        };
+//    public AlterarUsuarioDto GerarAlterarUsuarioDto() =>
+//        new()
+//        {
+//            Cpf = _faker.Random.Long(11111111111, 99999999999).ToString(),
+//            Id = _faker.Random.Guid(),
+//            Login = _faker.Random.String2(30),
+//            Nome = _faker.Random.String2(30),
+//            Senha = _faker.Random.String2(10)
+//        };
 
-    public AdicionarUsuarioDto GerarAdicionarUsuarioDto() =>
-        new (
-            _faker.Random.Long(11111111111, 99999999999).ToString(),
-            _faker.Random.String2(30),
-            Perfil.Avaliador,
-            _faker.Random.String2(30),
-            _faker.Random.String2(10));
+//    public AdicionarUsuarioDto GerarAdicionarUsuarioDto() =>
+//        new (
+//            _faker.Random.Long(11111111111, 99999999999).ToString(),
+//            _faker.Random.String2(30),
+//            Perfil.Avaliador,
+//            _faker.Random.String2(30),
+//            _faker.Random.String2(10));
 
-    public GerarTokenUsuarioDto GerarGerarTokenUsuarioDto() =>
-        new(_faker.Random.String2(30), _faker.Random.String2(10));
-}
+//    public GerarTokenUsuarioDto GerarGerarTokenUsuarioDto() =>
+//        new(_faker.Random.String2(30), _faker.Random.String2(10));
+//}
 
-[CollectionDefinition("UsuarioTestFixtureCollection")]
-public class UsuarioTestFixtureCollection : ICollectionFixture<UsuarioTestFixture>
-{ }
+//[CollectionDefinition("UsuarioTestFixtureCollection")]
+//public class UsuarioTestFixtureCollection : ICollectionFixture<UsuarioTestFixture>
+//{ }
